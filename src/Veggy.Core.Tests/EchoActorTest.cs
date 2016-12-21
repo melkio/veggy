0c26503f12b35329ba8bc0d92a28d200291af70d
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-12-21 16:05:28 +0100

    completed projects setup with packages and firt (fake) test

---
+++ src/Veggy.Core.Tests/EchoActorTest.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Actor;
+using Akka.TestKit.NUnit3;
+using NUnit.Framework;
+
+namespace Veggy.Core.Tests
+{
+    [TestFixture]
+    public class EchoActorTest : TestKit
+    {
+        [Test]
+        public void Should_echo_the_request()
+        {
+            const string text = "Hello world";
+
+            var request = new EchoActor.Request(text);
+            var actor = Sys.ActorOf(Props.Create<EchoActor>());
+
+            actor.Tell(request);
+
+            var response = ExpectMsg<EchoActor.Response>();
+            Assert.AreEqual(text, response.Text);
+        }
+    }
+}
---
+++ src/Veggy.Core/EchoActor.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+
+namespace Veggy.Core
+{
+    public class EchoActor : ReceiveActor
+    {
+        public EchoActor()
+        {
+            Receive<Request>(request => Sender.Tell(new Response(request.Text)));
+        }
+
+        public class Request
+        {
+            public string Text { get; }
+
+            public Request(string text)
+            {
+                Text = text;
+            }
+        }
+
+        public class Response
+        {
+            public string Text { get; }
+
+            public Response(string text)
+            {
+                Text = text;
+            }
+        }
+    }
+}
